8bb68df0f5af22b58b8883de3abeda8792569ebe
rajeev.jha <raj.k9732@gmail.com>
2018-04-14 00:41:00 +0530

    added student component
    Change-Id: Id443f8da9bf2d4e18a0e8dd8b4a6bb6f928dac06
    Signed-off-by: rajeev.jha <raj.k9732@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {CdkTableModule} from '@angular/cdk/table';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from "@angular/common/http";
+
 import {
   MatAutocompleteModule,
   MatButtonModule,
@@ -37,34 +38,46 @@ import {
   MatTabsModule,
   MatToolbarModule,
   MatTooltipModule,
-} from '@angular/material';
+}  from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import 'hammerjs';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
-import { StudentComponentComponent } from './component/student-component/student-component.component';
+import { StudentComponentComponent,DialogEditStudent } from './component/student-component/student-component.component';
 import { AppRoutingModule } from './app.routing.module';
+
 import {studentService} from './shared/service/studentservice/student.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    DialogEditStudent,
     StudentComponentComponent
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
-    MatToolbarModule,
     CdkTableModule,
-    MatExpansionModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatInputModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
     BrowserAnimationsModule,
     HttpClientModule,
-    AppRoutingModule
+    MatFormFieldModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [studentService],
+  entryComponents: [DialogEditStudent],
   bootstrap: [AppComponent]
 
 })
--- src/app/component/student-component/student-component.component.ts
+++ src/app/component/student-component/student-component.component.ts
@@ -1,8 +1,10 @@
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import {StudentDto} from "../../shared/model/student/student.model";
 import {studentService} from "../../shared/service/studentservice/student.service";
 import { Observable } from 'rxjs/Observable';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
 @Component({
   selector: 'app-student-component',
   templateUrl: './student-component.component.html',
@@ -10,7 +12,7 @@ import { Observable } from 'rxjs/Observable';
 })
 export class StudentComponentComponent implements OnInit {
   studentDtos:StudentDto[];
-  constructor(private studentService:studentService) { }
+  constructor(private studentService:studentService,private dialog:MatDialog) { }
 
   ngOnInit() {
     this.studentService.getAllStudents().subscribe(data=> {
@@ -18,5 +20,33 @@ export class StudentComponentComponent implements OnInit {
       });
   }
 
-  
+  openStudentDailog(studentDto:StudentDto): void {
+    let dialogRef = this.dialog.open(DialogEditStudent, {
+      height: '400px',
+      width: '600px',
+      data: { fname: studentDto.fname, lastname: studentDto.lastname,dob:new Date(studentDto.dob),
+        doj:new Date(studentDto.doj),session:studentDto.session,phone:studentDto.phone}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'student-edit-dialog.html',
+})
+export class DialogEditStudent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogEditStudent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
 }
